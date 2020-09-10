eafd179fd8d603aefa3225884c349c87fe81bc77
tejaswini <teju@PBC-SWD046304M.local>
2020-09-10 11:31:33 +0700

    samanth tool inital commit

---
+++ lib/samanth_view_tool.rb
@@ -0,0 +1,7 @@
+require "samanth_view_tool/version"
+require "samanth_view_tool/renderer"
+
+module SamanthViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/samanth_view_tool/version.rb
@@ -0,0 +1,3 @@
+module SamanthViewTool
+  VERSION = "0.1.0"
+end
